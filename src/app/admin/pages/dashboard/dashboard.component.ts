1b4b393f4e1cbcc9b5137fc881bd1b2a8ee8be19
Parminder Singh <parminder@datiot.com>
2018-11-01 10:24:33 +0530

    dashboard page dummy

--- src/app/admin/pages/dashboard/dashboard.component.ts
+++ src/app/admin/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ScriptLoaderService } from './../../../_services/script-loader.service';
+import { Component, OnInit, ViewEncapsulation, AfterViewInit } from '@angular/core';
 
 @Component({
-    selector: 'app-blank',
+    selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
     encapsulation: ViewEncapsulation.None,
 })
 export class DashboardComponent implements OnInit {
 
-    constructor() {
+    constructor(private _script: ScriptLoaderService) {
     }
 
     ngOnInit() {
+    }
+
+    ngAfterViewInit() {
+        this._script.loadScripts('app-index',
+            ['assets/app/js/dashboard.js']);
+
     }
 }
