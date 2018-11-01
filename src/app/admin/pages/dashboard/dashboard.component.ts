27ea87eb0f1b9c0ec01fa46c5e613f7537136378
Parminder Singh <parminder@datiot.com>
2018-11-01 11:01:17 +0530

    toatsr addeed

--- src/app/admin/pages/dashboard/dashboard.component.ts
+++ src/app/admin/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,15 @@
 import { ScriptLoaderService } from './../../../_services/script-loader.service';
-import { Component, OnInit, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, AfterViewInit, AfterContentInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
     encapsulation: ViewEncapsulation.None,
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit, AfterContentInit {
 
-    constructor(private _script: ScriptLoaderService) {
+    constructor(private _script: ScriptLoaderService, private toastr: ToastrService) {
     }
 
     ngOnInit() {
@@ -17,6 +18,11 @@ export class DashboardComponent implements OnInit {
     ngAfterViewInit() {
         this._script.loadScripts('app-index',
             ['assets/app/js/dashboard.js']);
+           
+    }
 
+    ngAfterContentInit() {
+        setTimeout(()=>this.toastr.success('Hello world!', 'Toastr fun!'))
+        
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,11 @@ import { SharedModule } from './shared/shared.module';
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+        ToastrModule.forRoot({
+            timeOut: 10000,
+            positionClass: 'toast-bottom-right',
+            preventDuplicates: true,
+        }),
         AppRoutingModule,
         SharedModule,
         AdminModule,
