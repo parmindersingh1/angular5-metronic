2402ccc93707eb7a3588100b3e549b5528c12f20
Parminder Singh <parminder@datiot.com>
2018-11-17 19:58:08 +0530

    menu issue fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Helpers } from "./helpers";
 })
 export class AppComponent implements OnInit {
     title = 'app';
-    globalBodyClass = 'm-page--loading-non-block m-page--fluid m--skin- m-content--skin-light2 m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default';
+    globalBodyClass = 'm-page--loading-non-block m-page--fluid m--skin- m-content--skin-light2 m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--fixed m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default';
 
     constructor(private _router: Router) {
     }
